cbc69735a3953c9ebd5e67c20289115504db74d1
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2024-10-07 17:12:19 -0300

    Update Story.js

--- clone-instagram/src/componentes/Story.js
+++ clone-instagram/src/componentes/Story.js
@@ -4,6 +4,7 @@ export default function Story() {
             <div className='imagem-do-usuario'>
                 <img src='https://picsum.photos/200/300' alt='Imagem do usuário'/>
             </div>
+            
             <div className='nome-do-usuario'>
                 <span>sleeap</span>
             </div>
